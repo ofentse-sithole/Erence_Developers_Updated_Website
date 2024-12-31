b29bed604a6f1c0f82401680e12580bb2c86cf18
Ofentse Sithole <ST10104487@rcconnect.edu.za>
2024-12-31 12:30:48 +0200

    added about feature section along with company profile making it download

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,10 +1,12 @@
 import '../../App.css'
+import AboutSection from '../FeatureSection/AboutFeatureSection'
 import AboutHeroSection from '../HeroSection/AboutHeroSection'
 
 const About = () => {
     return (
         <div>
             <AboutHeroSection />
+            <AboutSection/>
         </div>
     )
 }

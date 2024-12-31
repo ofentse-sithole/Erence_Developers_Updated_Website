651c24ebfc7cb9d7177faef80c9d149c37b084c2
Ofentse Sithole <ST10104487@rcconnect.edu.za>
2024-12-31 11:55:56 +0200

    edited the service and made it have only featureservice

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,7 +1,6 @@
 import '../../App.css'
 import FeatureServices from '../FeatureSection/ServicesFeature';
 import ServicesHeroSection from '../HeroSection/ServiceHeroSection'
-import IconFeatureSection from '../FeatureSection/IconFeatureSection';
 
 const Services = () => {
     return (
